e84ccaf0580803404ad9d03cab5538ad16306f97
Shahrzadzhr <sh.zahrayi@gmail.com>
2024-05-13 14:15:43 +0200

    aktualisieren

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,9 +1,11 @@
 
 import 'package:flutter/material.dart';
 
-
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+   LoginScreen({super.key});
+
+  // This state should ideally be in a StatefulWidget or managed via a state management solution in real applications
+  final ValueNotifier<bool> _rememberMe = ValueNotifier(false);
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +16,25 @@ class LoginScreen extends StatelessWidget {
             // Background image with opacity
             SizedBox.expand(
               child: Opacity(
-                opacity: 0.9, 
-                child: Image.asset(".idea/assets/backgroundimage.jpg", fit: BoxFit.cover),
+                opacity: 0.9,
+                child: Image.asset("assets/backgroundimage.jpg", fit: BoxFit.cover),
+              ),
+            ),
+            Positioned(
+              top: 32,
+              left: 8,
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back_ios),
+                color: Colors.white,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
             ),
 
-
             // Positioned logo
             Positioned(
-              top: -10, 
+              top: -10,
               right: 0,
               left: 0,
               bottom: 400,
@@ -33,14 +45,13 @@ class LoginScreen extends StatelessWidget {
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(35.0),
                     image: DecorationImage(
-                      image: AssetImage(".idea/assets/logo.png"),
+                      image: AssetImage("assets/logo.png"),
                       fit: BoxFit.cover,
                     ),
                   ),
                 ),
               ),
             ),
-
 
             // Positioned login form
             Positioned(
@@ -50,139 +61,124 @@ class LoginScreen extends StatelessWidget {
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    Container(
-                      width: 322,
-                      height: 71,
-                      decoration: ShapeDecoration(
-                        color: Colors.white.withOpacity(0.6),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(13),
-                        ),
-                      ),
-
-
-                      alignment: Alignment.topLeft,
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          labelText: 'Username, email or mobile number',
-                          labelStyle: TextStyle(
-                            color: Color(0xFF892625),
-                            fontSize: 14,
-                            fontFamily: 'SF Pro',
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                    ),
-                    
-
+                    // Username or email input
+                    buildTextInput("Username, email or mobile number", false),
                     SizedBox(height: 14),
+                    // Password input
+                    buildTextInput("Password", true),
+
+                    SizedBox(height: 20),
+                    // Login button
+                    buildLoginButton(),
+
+                    SizedBox(height: 8),
+                    // Remember me and Forgot password
+                    buildRememberMe(),
 
                     Container(
-                      width: 322,
-                      height: 71,
-                      decoration: ShapeDecoration(
-                        color: Colors.white.withOpacity(0.6),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(13),
-                        ),
-                      ),
-
-                      alignment: Alignment.topLeft,
-                      child: TextFormField(
-                        obscureText: true, 
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          labelText: 'Password',
-                          labelStyle: TextStyle(
-                            color: Color(0xFF892625),
-                            fontSize: 14,
-                            fontFamily: 'SF Pro',
-                            fontWeight: FontWeight.w500,
-                          ),
-
-                          // visibility off icon 
-                          suffixIcon: Icon(Icons.visibility_off),
-                         
-                        ),
-                      ),
-                    ),
-
-                    SizedBox(height: 30),
-
-                    Container(
-                      width: 324,
-                      height: 54,
-                      decoration: ShapeDecoration(
-                        color: const Color(0xFFA63533),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(26.78),
-                        ),
-                      ),
-
-
-                      alignment: Alignment.center,
-                      child: Text(
-                        'LOG IN',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontFamily: 'SF Pro',
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                    
-                    
-                    SizedBox(height: 25),
-                    
-                    
-                    const Text(
-                    'Remember me',
-                      style: TextStyle(
-                         color: Colors.white,
-                          fontSize: 15,
-                       fontFamily: 'SF Pro',
-                       fontWeight: FontWeight.w600,
-                       
-                     ),
-                    ),
-
-
-                       Container(
                        width: 178,
                       height: 1,
                       decoration: BoxDecoration(color: Colors.white),
                     ),
 
-
-                       SizedBox(height: 3),
-
-                       const Text(
-                       'Forgot password?',
-                       style: TextStyle(
-                       color: Colors.white,
-                       fontSize: 15,
-                      fontFamily: 'SF Pro',
-                      fontWeight: FontWeight.w600,
-
+                    const Text(
+                      'Forgot password?',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontFamily: 'SF Pro',
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
-                  ),
-
-                ],
-                
-   
+                  ],
                 ),
-               ),
+              ),
             ),
-          ]),
+          ],
         ),
-      );
-    
-    
+      ),
+    );
+  }
+
+  Widget buildTextInput(String label, bool obscureText) {
+    return Container(
+      width: 322,
+      height: 71,
+      decoration: ShapeDecoration(
+        color: Colors.white.withOpacity(0.6),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(13),
+        ),
+      ),
+      alignment: Alignment.topLeft,
+      child: TextFormField(
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          labelText: label,
+          labelStyle: TextStyle(
+            color: Color(0xFF892625),
+            fontSize: 14,
+            fontFamily: 'SF Pro',
+            fontWeight: FontWeight.w500,
+          ),
+          suffixIcon: obscureText ? const Icon(Icons.visibility_off) : null,
+        ),
+      ),
+    );
+  }
+
+  Widget buildLoginButton() {
+    return Container(
+      width: 324,
+      height: 54,
+      decoration: ShapeDecoration(
+        color: const Color(0xFFA63533),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(26.78),
+        ),
+      ),
+      alignment: Alignment.center,
+      child: const Text(
+        'LOG IN',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontFamily: 'SF Pro',
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    );
+  }
+
+  Widget buildRememberMe() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ValueListenableBuilder<bool>(
+          valueListenable: _rememberMe,
+          builder: (_, value, __) {
+            return Checkbox(
+              value: value,
+              onChanged: (newValue) {
+                _rememberMe.value = newValue ?? false;
+              },
+              checkColor: Color.fromARGB(255, 161, 29, 29), // color of tick Mark
+              activeColor: const Color.fromARGB(255, 255, 255, 255),
+            );
+          },
+        ),
+        
+        const Text(
+          'Remember me',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+            fontFamily: 'SF Pro',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
+    );
   }
 }
-
-
-
