f792745d2c2bea8661782a73c642af0532970c92
Shahrzadzhr <sh.zahrayi@gmail.com>
2024-06-26 15:26:41 +0200

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_project_modame/firebase_options.dart';
 import 'package:my_project_modame/src/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
